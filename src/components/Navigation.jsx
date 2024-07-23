c41fe9c04b0788f85d1adcb7fe6b1b519f42ce34
Ma Bahagia <ma.bahagia@gmail.com>
2024-07-23 12:56:31 +0700

    fix: collapse navigation after click menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,14 @@ import { Dropdown } from "bootstrap";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 import Cookies from "universal-cookie";
+import react, { useRef } from "react";
 
 function Navigation({ dataUser }) {
+  const toggle = useRef(null);
+
+  const onClickMenu = () => {
+    toggle.current.click();
+  };
   return (
     <>
       <Navbar
@@ -15,39 +21,77 @@ function Navigation({ dataUser }) {
       >
         <Container>
           <Navbar.Brand href="">Backoffice MA</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" ref={toggle} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/" type="button" className="nav-link">
+              <Link
+                to="/"
+                type="button"
+                className="nav-link"
+                onClick={() => onClickMenu()}
+              >
                 Dashboard
               </Link>
               <NavDropdown title="Produk" id="basic-nav-dropdown">
-                <Link to="produk/data-produk" className="dropdown-item">
+                <Link
+                  to="produk/data-produk"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Data Produk
                 </Link>
-                <Link to="produk/kategori" className="dropdown-item">
+                <Link
+                  to="produk/kategori"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Kategori
                 </Link>
-                <Link to="produk/penyesuaian-stok" className="dropdown-item">
+                <Link
+                  to="produk/penyesuaian-stok"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Penyesuaian Stok
                 </Link>
               </NavDropdown>
               <NavDropdown title="Laporan" id="basic-nav-dropdown">
-                <Link to="laporan/penjualan" className="dropdown-item">
+                <Link
+                  to="laporan/penjualan"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Penjualan
                 </Link>
-                <Link to="laporan/transaksi" className="dropdown-item">
+                <Link
+                  to="laporan/transaksi"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Transaksi
                 </Link>
               </NavDropdown>
-              <Link to="/pelanggan" type="button" className="nav-link">
+              <Link
+                to="/pelanggan"
+                type="button"
+                className="nav-link"
+                onClick={() => onClickMenu()}
+              >
                 Pelanggan
               </Link>
               <NavDropdown title="Pengaturan" id="basic-nav-dropdown">
-                <Link to="pengaturan/akun" className="dropdown-item">
+                <Link
+                  to="pengaturan/akun"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Akun
                 </Link>
-                <Link to="pengaturan/nota" className="dropdown-item">
+                <Link
+                  to="pengaturan/nota"
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   Nota
                 </Link>
               </NavDropdown>
@@ -55,7 +99,11 @@ function Navigation({ dataUser }) {
 
             <Nav className="ms-auto">
               <NavDropdown title={dataUser.nama} id="basic-nav-dropdown">
-                <Link to={"/profil/" + dataUser.nama} className="dropdown-item">
+                <Link
+                  to={"/profil/" + dataUser.nama}
+                  className="dropdown-item"
+                  onClick={() => onClickMenu()}
+                >
                   {dataUser.username}
                 </Link>
                 <Link
